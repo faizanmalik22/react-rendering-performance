ab26eb713cadfb0bc3db7fc984e47aa6da6a8d15
Faizan malik <faizanislam5425@gmail.com>
2026-09-12 03:42:43 +0530

    Add children prop composition example

--- src/App.jsx
+++ src/App.jsx
@@ -4,26 +4,25 @@ import SlowComponent from './Component/SlowComponent/SlowComponent';
 import Modal from './Component/Modal/Modal';
 import ButtonwithModal from './Component/ButtonWithModal/ButtonWithModal';
 import useModalDialog from './Hooks/useModalDialog';
-
-function Counter(){
-  //Sol:State colocation : keeping the counter state x to close to button
-    const [x,Setx]=useState(0);//belong to counter
-    return (
-      // UI of the Counter
-      <button onClick={()=>Setx(x+1)}>counter {x}</button>
-    )
-}
-
+import RefactorComponent from './Component/RefactorComponent/RefactorComponent';
 function App() {
- 
+
   return (
     <>
    {/* Import the counter here so App doesnt contain or uses x so no re-render for App */}
-      <Counter/>
-      <h1>Something done here</h1>
-      <div>Somthing also done here</div>
-      <SlowComponent/>
-      <ButtonwithModal/>
+       {/* <button onClick={()=>Setx(x+1)}>counter {x}</button> */}
+      
+       
+        <RefactorComponent>
+         {/* pass as child props  so when Re-factor re-render this child component will not re-render*/}
+         <>
+           <h1>Something done here</h1>
+          <div>Somthing also done here</div>
+          <SlowComponent ms={1000}/>
+         </>
+        </RefactorComponent>
+     
+       {/* <ButtonwithModal/> */}
     </>
   );
 }
---
+++ src/Component/RefactorComponent/RefactorComponent.jsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+function RefactorComponent({children}){
+const [x,Setx]=useState(0);//belong to counter
+return(
+    <>
+    <button onClick={()=>{Setx(x+1)}}>
+     {x}
+    </button>
+    {/* below button on this component lets we want to render child props component. */}
+    {/* now when state change Refactor re-render but this children props (child component) will not re-render. i know why */}
+    {children}
+   </>
+);
+}
+export default RefactorComponent;
